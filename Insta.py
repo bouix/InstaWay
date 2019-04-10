8d31df960fc7969fdd87a1a56f86a001e7f25a27
Mickaël Bouix <49488324+bouix@users.noreply.github.com>
2019-04-10 12:40:10 -0300

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -16,17 +16,17 @@ try:
     element_present = EC.presence_of_element_located((By.ID, 'element_id'))
     WebDriverWait(driver, timeout).until(element_present)
 except TimeoutException:
-    print "Carregando Log in..."
+    print "Loading Log in..."
 
 driver.find_element_by_xpath("//input[@name='username']").send_keys("USERNAME")
 driver.find_element_by_xpath("//input[@name='password']").send_keys("PASSWORD")
-driver.find_element_by_xpath("//button[contains(.,'Entrar')]").click()
+driver.find_element_by_xpath("//button[contains(.,'Log In')]").click()
 sleep( 60 )
 try:
     element_present = EC.presence_of_element_located((By.ID, 'element_id'))
     WebDriverWait(driver, timeout).until(element_present)
 except TimeoutException:
-    print "Nome Armazenado..."
+    print "Scrapped..."
 driver.get("https://www.instagram.com/p/LINK/")
 
 
@@ -35,7 +35,7 @@ f = open("mentions.txt", "r")
 for x in f:
     
         driver.find_element_by_xpath("//span[@aria-label='Comentar']").click()
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//form[@class='X7cDz']//textarea[contains(@placeholder,'Adicione')]"))).send_keys(x)
-        driver.find_element_by_xpath("//textarea[contains(@aria-label,'Adicione')]").send_keys(Keys.ENTER)
-        print "Marcado:",x
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//form[@class='X7cDz']//textarea[contains(@placeholder,'Add')]"))).send_keys(x)
+        driver.find_element_by_xpath("//textarea[contains(@aria-label,'Add')]").send_keys(Keys.ENTER)
+        print "Tagged:",x
         sleep( 60 )
